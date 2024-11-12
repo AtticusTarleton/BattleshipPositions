128bc4791391a757ebc36b583143ab6a1e6bc573
AtticusTarleton <atarleto@uvm.edu>
2024-11-12 13:35:46 -0500

    added board and ship files

---
+++ board.h
@@ -0,0 +1,12 @@
+//
+// Created by Atticus Tarleton on 11/12/24.
+//
+
+#ifndef BOARD_H
+#define BOARD_H
+//this will create the board
+
+
+
+
+#endif //BOARD_H
---
+++ ship.h
@@ -0,0 +1,12 @@
+//
+// Created by Atticus Tarleton on 11/12/24.
+//
+
+#ifndef SHIP_H
+#define SHIP_H
+//this will create ships of different sizes
+
+
+
+
+#endif //SHIP_H
